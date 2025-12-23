378597299aee9fc17635391119829c6fd8f1b2ab
Arun <arunmozhi.g@gmail.com>
2025-12-23 19:14:21 +1100

    Renamed variables, avoided duplicate code and moved a block of code

--- CostCalculator/Program.cs
+++ CostCalculator/Program.cs
@@ -5,13 +5,17 @@ Console.WriteLine("\n");
 const int TEAM_AVERAGE_FLOORING_SPEED_PER_HOUR = 20;
 const int TEAM_COST_PER_HOUR = 86;
 const double PI = 3.14159265358979;
+const int RECTANGLE_FLOOR = 1;
+const int TRIANGLE_FLOOR = 2;
+const int CIRCLE_FLOOR = 3;
+const int EXIT = 0;
 
 int choiceNumber = 5;
-double cost_for_flooring;
-double area_of_the_floor;
-double time_required_for_flooring;
+double costOfFlooring;
+double areaOfTheFloor = 0.0;
+double timeRequiredForFlooring;
 
-while (choiceNumber != 0)
+while (choiceNumber != EXIT)
 {
     Console.WriteLine("Choose the shape of the floor from the list below: ");
     Console.WriteLine(" 1 - Rectangle");
@@ -21,7 +25,13 @@ while (choiceNumber != 0)
 
     choiceNumber = Convert.ToInt32(Console.ReadLine());
 
-    if (choiceNumber == 1)
+    if (choiceNumber != EXIT && choiceNumber != RECTANGLE_FLOOR &&  choiceNumber != CIRCLE_FLOOR && choiceNumber != TRIANGLE_FLOOR)
+    {
+        Console.WriteLine("Wrong Input, please try again");
+        continue;
+    }
+
+    if (choiceNumber == RECTANGLE_FLOOR)
     {
         Console.WriteLine("Enter the width of the Rectangular floor: ");
         int width = Convert.ToInt32(Console.ReadLine());
@@ -29,14 +39,10 @@ while (choiceNumber != 0)
         Console.WriteLine("Enter the length of the Rectangular floor: ");
         int length = Convert.ToInt32(Console.ReadLine());
 
-        area_of_the_floor = width * length;
-        time_required_for_flooring = area_of_the_floor / TEAM_AVERAGE_FLOORING_SPEED_PER_HOUR;
-        cost_for_flooring = TEAM_COST_PER_HOUR * time_required_for_flooring;
-
-        Console.WriteLine($"Cost for flooring: $ {cost_for_flooring.ToString("F2")}");
+        areaOfTheFloor = width * length;
     }
 
-    if (choiceNumber == 2)
+    if (choiceNumber == TRIANGLE_FLOOR)
     {
         Console.WriteLine("Enter the base of the Triangular floor: ");
         int baseLength = Convert.ToInt32(Console.ReadLine());
@@ -44,29 +50,21 @@ while (choiceNumber != 0)
         Console.WriteLine("Enter the height of the Triangular floor: ");
         int height = Convert.ToInt32(Console.ReadLine());
 
-        area_of_the_floor = 0.5 * baseLength * height;
-        time_required_for_flooring = area_of_the_floor / TEAM_AVERAGE_FLOORING_SPEED_PER_HOUR;
-        cost_for_flooring = TEAM_COST_PER_HOUR * time_required_for_flooring;
-
-        Console.WriteLine($"Cost for flooring: $ {cost_for_flooring.ToString("F2")}");
+        areaOfTheFloor = 0.5 * baseLength * height;
     }
 
-    if (choiceNumber == 3)
+    if (choiceNumber == CIRCLE_FLOOR)
     {
         Console.WriteLine("Enter the radius of the Circular floor in centimeters: ");
         int radius = Convert.ToInt32(Console.ReadLine());
 
-        area_of_the_floor = PI * radius * radius;
-        time_required_for_flooring = area_of_the_floor / TEAM_AVERAGE_FLOORING_SPEED_PER_HOUR;
-        cost_for_flooring = TEAM_COST_PER_HOUR * time_required_for_flooring;
-
-        Console.WriteLine($"Cost for flooring: $ {cost_for_flooring.ToString("F2")}");
+        areaOfTheFloor = PI * radius * radius;
     }
 
-    if (choiceNumber < 0 || choiceNumber > 3)
-    {
-        Console.WriteLine("Wrong Input, please try again");
-    }
+    timeRequiredForFlooring = areaOfTheFloor / TEAM_AVERAGE_FLOORING_SPEED_PER_HOUR;
+    costOfFlooring = TEAM_COST_PER_HOUR * timeRequiredForFlooring;
+
+    Console.WriteLine($"Cost for flooring: $ {costOfFlooring.ToString("F2")}");
 }
 
 Console.WriteLine("Thanks for using the Flooring CostCalculator Program");
